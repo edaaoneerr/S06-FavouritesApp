899891d3fc02fc2f3217ca74ce6400f907646def
edaaoneerr <edaaoneerr@gmail.com>
2023-04-21 22:54:59 +0300

    Initial Commit

---
+++ FavouriteBook/FavouriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteBookApp.swift
+//  FavouriteBook
+//
+//  Created by Eda Oner on 21.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
